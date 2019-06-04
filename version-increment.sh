94effbf5a77af87d3eb02f4e4c4b03379b4ff0ce
Laura Meikle <laura.s.meikle@gmail.com>
2019-06-04 16:17:21 +0100

    addedxggdddsdfdfg

--- version-increment.sh
+++ version-increment.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-CURRENT_VERSION=$(npm run version --silent)
-echo $CURRENT_VERSION
-
 git update-index -q --refresh
 if ! git diff-index --quiet HEAD --; then
   echo "Working directory not clean, please commit your changes first"
@@ -10,6 +7,8 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 npm --no-git-tag-version version patch
+
+CURRENT_VERSION=$(npm run version --silent)
 
 git add .
 git commit -m "Publish " + $CURRENT_VERSION
